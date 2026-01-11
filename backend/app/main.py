81bd62f21f8b479ec679f12dcd504cbb5a947202
Vasanth Gondrala <vasanthvs.7032@gmail.com>
2026-01-11 22:03:08 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:5173",
-        "https://ai-wiki-quiz-generator-delta.vercel.app"
+        "https://ai-wiki-quiz-plum.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
